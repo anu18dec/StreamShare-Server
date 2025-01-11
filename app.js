b572e2d96ef9c2d4cd6e9541da4b7fb220d2ffd4
Anupam Patel <dec18anu@gmail.com>
2025-01-11 13:14:46 +0530

    Fix cors error

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import SocketServices from "./services/socketServices.js";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 const appServer = http.createServer(app);
 
+dotenv.config({ path: ".env" });
+
 const io = new Server(appServer, {
     cors: {
-        origin: "*",
+        origin: process.env.CLIENT_URL,
         methods: ["GET", "POST"],
         credentials: true,
     },
 });
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.send("Server is running...");
